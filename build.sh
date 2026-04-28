f27150ec109944a1b6b5c8f6e4acef111cf78d0a
cmciosu <167565292+cmciosu@users.noreply.github.com>
2026-04-28 14:06:10 -0700

    Update build.sh
    bypass "./build.sh exited with 1" error when attempting to deploy

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 python3 --version
 
 echo "Building the project..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
